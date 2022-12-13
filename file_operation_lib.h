c9213df0b7758252f9bbc539df56d672780d3343
pmalec1 <01169056@pw.edu.pl>
2022-12-13 22:58:01 +0100

    Update  header file - new definitions of functions from file_operation_library.c

--- file_operation_lib.h
+++ file_operation_lib.h
@@ -1,21 +1,25 @@
 
-
 #include <stdio.h>
-
+#include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
-#define true 1
-#define false 0
+#include <stdbool.h>
+#define new_size_of_array 9
 
 
 #define NUMBER_OF_EXPECTED_EXT_ARGS 3
-#define SIZE_OF_ARRAY_OF_NUMBERS    20
 
-void read_numbers_from_file(int array_of_numbers[SIZE_OF_ARRAY_OF_NUMBERS],short amount_of_numbers);
+
+
 int check_if_file_exists(char path_to_file[255]);
 int check_number_of_arguments(int number_of_calling_args);
+int write_number_into_array( int Array_of_numbers[]);
 
 void message_correct_no_of_args();
 void message_incorrect_no_of_args();
 void message_file_exist();
 void message_file_not_exist();
+bool check_if_there_are_any_letters();
+void message_about_letters_in_file();
+int  count_numbers_in_the_file();
+
