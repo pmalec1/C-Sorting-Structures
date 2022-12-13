3848f2fa1e4481f0069d73bd10db80924ca5e6bc
pmalec1 <01169056@pw.edu.pl>
2022-12-13 22:56:24 +0100

    New functions in lib

--- file_operation_lib.c
+++ file_operation_lib.c
@@ -1,28 +1,70 @@
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
 #include "file_operation_lib.h"
 extern FILE *myfile;
 
 
-
-void read_numbers_from_file(int array_of_numbers[SIZE_OF_ARRAY_OF_NUMBERS],short amount_of_numbers)
+int count_numbers_in_the_file()
 {
-    int temp_number;
-    int index_in_array = 0;
-    while(index_in_array<SIZE_OF_ARRAY_OF_NUMBERS)
-    {
-    fscanf(myfile,"%d",&temp_number);
-    if(index_in_array == amount_of_numbers) break;
-    array_of_numbers[index_in_array]=temp_number; 
-    printf("%d\n",temp_number);
-    fseek(myfile,1,1);
-    ++index_in_array;
+ int znak = 0;
+ int amount_of_numbers = 0;
+ 
+ while(znak!=EOF)
+ {   
+    znak = getc(myfile); 
+    if(isdigit(znak)>0)
+    {   znak = getc(myfile); 
+        if(ispunct(znak)>0) ++amount_of_numbers;
+        else 
+        {   if(znak==EOF)++amount_of_numbers;
+            fseek(myfile,1,1);
+        }
     }
+  } 
+  return amount_of_numbers;
+}
 
+
+bool check_if_there_are_any_letters()
+{
+ int ilosc_bledow =0;
+ int znak = 0;
+    while((znak!=EOF))
+    {
+    znak = getc(myfile);
+    if(isalpha(znak)>0)
+    {
+        ++ilosc_bledow;
+    }
+    }
+       if(ilosc_bledow!=0)return true;
+    else return false;
+}
+
+void message_about_letters_in_file()
+{
+    printf("INCORRECT FILE FORMAT\n");
+    printf("The file cannot contain letters\n");
+}
+
+int write_number_into_array( int Array_of_numbers[new_size_of_array])
+{
+ int temp_number = 0;
+ int index_in_array = 0;
+ fseek(myfile,0,0);
+ while(index_in_array<new_size_of_array)
+ {
+    fscanf(myfile,"%d",&temp_number);
+    fseek(myfile,1,1);
+    Array_of_numbers[index_in_array]=temp_number;
+    ++index_in_array; 
+ }
 
 }
+
+
+
+
 void message_correct_no_of_args()
 {
     printf("Number of arguments is correct :D\n");
