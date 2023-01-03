9e88a2a77068e847bebf9dd005bbee380851efef
pmalec1 <01169056@pw.edu.pl>
2023-01-03 21:08:58 +0100

    Replace file_operation_lib.h

--- file_operation_lib.h
+++ file_operation_lib.h
@@ -1,25 +1,23 @@
-
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
-#define new_size_of_array 9
 
+#define NUMBEROFCHARS 128
 
-#define NUMBER_OF_EXPECTED_EXT_ARGS 3
+typedef struct
+{
+    int id;
+    char name[NUMBEROFCHARS];
+    int age;
+    float height;
+} Humans_struct;
 
-
-
-int check_if_file_exists(char path_to_file[255]);
-int check_number_of_arguments(int number_of_calling_args);
-int write_number_into_array( int Array_of_numbers[]);
-
-void message_correct_no_of_args();
-void message_incorrect_no_of_args();
-void message_file_exist();
-void message_file_not_exist();
-bool check_if_there_are_any_letters();
-void message_about_letters_in_file();
-int  count_numbers_in_the_file();
-
+void get_new_line(char temp_line[NUMBEROFCHARS]);
+Humans_struct *read_list_of_humans(int amount_of_humans);
+void get_operation_code(char sorting_category[NUMBEROFCHARS],short *ptr_to_operation_code);
+int count_humans();
+void copy_arguments_to_chars(char **argv,char path_to_file[NUMBEROFCHARS],char sorting_category[NUMBEROFCHARS]);
+void display_structs(Humans_struct *humans,unsigned int amount_of_structs);
+void bubble_sort_by_age(Humans_struct *humans,unsigned int amount_of_structs);
+void bubble_sort_by_height(Humans_struct *humans,unsigned int amount_of_structs);
+void bubble_sort(Humans_struct *humans,short operation_code,unsigned int amount_of_structs);
